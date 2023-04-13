42e96952d4befcaf69e5688f0c4c38f62c5d3999
Karthik Rangarajan <karthik@openai.com>
2023-04-13 11:02:40 -0700

    remove reference to google storage

--- lm_human_preferences/datasets/books.py
+++ lm_human_preferences/datasets/books.py
@@ -7,7 +7,7 @@ from lm_human_preferences.utils import gcs
 def books_generator(mode, seed=0, shuffle=False, comm=None):
     datas = [
         json.loads(line) for line in
-        open(gcs.download_file_cached(f'gs://lm-human-preferences/datasets/book_passages/{mode}.jsonl', comm=comm))
+        open(gcs.download_file_cached(f'https://openaipublic.blob.core.windows.net/lm-human-preferences/datasets/book_passages/{mode}.jsonl', comm=comm))
     ]
     if shuffle:
         random.seed(seed)
